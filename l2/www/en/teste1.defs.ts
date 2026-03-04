ea6214fb530a4f9627300c5049b397bc1fdad914
Lucas Mafra <36516904+lucasexpansiva@users.noreply.github.com>
2026-03-04 14:45:53 -0300

    group-1
    group-1

---
+++ l2/www/en/teste1.defs.ts
@@ -0,0 +1,2 @@
+/// <mls fileReference="_102031_/l2/www/en/teste1.defs.ts" enhancement="_blank"/>
+
---
+++ l2/www/en/teste1.ts
@@ -0,0 +1,15 @@
+/// <mls fileReference="_102031_/l2/www/en/teste1.ts" enhancement="_100554_enhancementLit"/>
+
+ import { html } from 'lit'; 
+ import { customElement, property } from 'lit/decorators.js';
+ import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
+
+ @customElement('www--en--teste1-102031')
+ export class Teste1102031 extends StateLitElement {
+    
+     @property() name: string = 'Somebody';
+
+     render() {
+         return html`<p> Hello, ${ this.name } !</p>`;
+     }
+ }
